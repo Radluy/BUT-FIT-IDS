cfa975e9e5f7b93a62fe053ef7e36fd8a3df7231
Filip Macák <filip.macak1999@gmail.com>
2020-04-17 13:56:55 +0200

    Asi hotovo

--- trigger.sql
+++ trigger.sql
@@ -8,9 +8,9 @@ DROP TABLE Grimoar_je_vlastneny CASCADE CONSTRAINTS ;
 DROP TABLE Miesto_s_magiou CASCADE CONSTRAINTS ;
 DROP TABLE Ucitel CASCADE CONSTRAINTS ;
 DROP SEQUENCE PKseq;
+DROP MATERIALIZED VIEW kuzlo_element;
 
 --Create--
-
 CREATE TABLE Ucitel(
     ID int PRIMARY KEY
 );
@@ -178,6 +178,123 @@ INSERT INTO Ucitel(ID)
 INSERT INTO GRIMOAR_JE_VLASTNENY(Grimoar_id, Kuzelnik_id, Od, Do)
 VALUES (3, 3, DATE '2020-10-10', DATE '2015-06-26');
 
+
+--procedures--
+
+-- procedura vypise pocet kuzelnikov, ucitelov, synergi a vztahy medzi nimi
+CREATE OR REPLACE PROCEDURE pocet_kuzelnikov_a_synergi
+AS
+    pocet_kuzelnikov NUMBER;
+    pocet_ucitelov NUMBER;
+    pocet_synergi NUMBER;
+    pocet_kuzelnikov_na_ucitela NUMBER;
+    pocet_synergi_na_kuzelnika NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO pocet_kuzelnikov FROM Kuzelnik;
+    SELECT COUNT(*) INTO pocet_ucitelov FROM Ucitel;
+    SELECT COUNT(*) INTO pocet_synergi FROM Synergia;
+
+    pocet_kuzelnikov_na_ucitela := pocet_kuzelnikov / pocet_ucitelov;
+    pocet_synergi_na_kuzelnika := pocet_synergi / pocet_kuzelnikov;
+
+    DBMS_OUTPUT.PUT_LINE('Pocet kuzelnikov: ' || pocet_kuzelnikov
+                            || ', Pocet ucitelov: ' || pocet_ucitelov
+                            || ', Pocet synergi: ' || pocet_synergi);
+
+    DBMS_OUTPUT.PUT_LINE('Pocet kuzelnikov na jedneho ucitela: ' || pocet_kuzelnikov_na_ucitela);
+    DBMS_OUTPUT.PUT_LINE('Pocet synergi na jedneho kuzelnika: ' || pocet_synergi_na_kuzelnika);
+
+    EXCEPTION WHEN ZERO_DIVIDE THEN
+    BEGIN
+        IF pocet_ucitelov = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('Ziadny ucitelia v databaze');
+        end if;
+
+        IF pocet_kuzelnikov = 0 THEN
+            DBMS_OUTPUT.PUT_LINE('Ziadny kuzelnici v databaze');
+        end if;
+    end;
+end;
+
+-- procedura spocita kolko grimoarov obsahuje dane kuzlo
+CREATE OR REPLACE PROCEDURE pocet_grimoarov_s_kuzlom(id_kuzlo IN int)
+AS
+    pocet_grimoarov NUMBER;
+    grimoary_s_kuzlom NUMBER;
+    kuzlo_id Kuzlo.ID%TYPE;
+    hladane_kuzlo Kuzlo.ID%TYPE;
+    CURSOR kuzla IS SELECT Kuzlo_id FROM Grimoar_obsahuje;
+BEGIN
+    SELECT COUNT(*) INTO pocet_grimoarov FROM Grimoar;
+    grimoary_s_kuzlom := 0;
+
+    SELECT ID INTO hladane_kuzlo FROM Kuzlo WHERE ID = id_kuzlo;
+
+    OPEN kuzla;
+    LOOP
+        FETCH kuzla INTO kuzlo_id;
+        EXIT WHEN kuzla%NOTFOUND;
+
+        IF kuzlo_id = hladane_kuzlo THEN
+            grimoary_s_kuzlom := grimoary_s_kuzlom + 1;
+        end if;
+    end loop;
+    CLOSE kuzla;
+
+    DBMS_OUTPUT.PUT_LINE(grimoary_s_kuzlom || ' grimoar(ov) obsahuje kuzlo ' || hladane_kuzlo);
+
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Kuzlo ' || id_kuzlo || ' nebolo najdene');
+    end;
+end;
+
+--priklady spusteni procedur
+BEGIN pocet_kuzelnikov_a_synergi(); END;
+BEGIN pocet_grimoarov_s_kuzlom(2); END;
+
+
+--explain plan--
+--ktory kuzelnici s urovnou pod 60 vlastnia grimoar a kolko ich vlastnia
+EXPLAIN PLAN FOR
+SELECT "k".Uroven AS uroven,
+       COUNT("g".Grimoar_id) AS pocet_grimoarov
+FROM Kuzelnik "k"
+JOIN Grimoar_je_vlastneny "g" ON "g".Kuzelnik_id = "k".ID
+WHERE "k".Uroven < 60
+GROUP BY "k".ID, "k".Uroven
+ORDER BY "k".Uroven;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
+
+--pokus s vytvorenym indexom
+CREATE INDEX uroven_kuzelnika ON Kuzelnik (Uroven);
+
+EXPLAIN PLAN FOR
+SELECT "k".Uroven AS uroven,
+       COUNT("g".Grimoar_id) AS pocet_grimoarov
+FROM Kuzelnik "k"
+JOIN Grimoar_je_vlastneny "g" ON "g".Kuzelnik_id = "k".ID
+WHERE "k".Uroven < 60
+GROUP BY "k".ID, "k".Uroven
+ORDER BY "k".Uroven;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
+
+
+--materialized view--
+--materializovany pohlad na kuzla a ich elementy
+CREATE MATERIALIZED VIEW kuzlo_element AS
+SELECT
+    "k".ID,
+    "k".Zlozitost,
+    "k".Sila,
+    "e".Specializacia
+FROM Kuzlo "k"
+JOIN Element "e" ON "e".ID = "k".Hlavny_element
+GROUP BY "k".ID, "k".Zlozitost, "k".Sila, "e".Specializacia;
+
+SELECT * FROM kuzlo_element;
+
+
 --grant permissions--
 
 GRANT ALL ON Ucitel TO xmacak07;
@@ -189,6 +306,6 @@ GRANT ALL ON Grimoar_obsahuje TO xmacak07;
 GRANT ALL ON Grimoar_je_vlastneny TO xmacak07;
 GRANT ALL ON Grimoar TO xmacak07;
 GRANT ALL ON Element TO xmacak07;
-
---procedures--
-
+GRANT EXECUTE ON pocet_kuzelnikov_a_synergi TO xmacak07;
+GRANT EXECUTE ON pocet_grimoarov_s_kuzlom TO xmacak07;
+GRANT ALL ON kuzlo_element TO xmacak07;
